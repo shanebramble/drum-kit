4eca1950f496bdcda61b1a8ad49cb8ba60da3fb8
Shane Laptop <sbramwade@msn.com>
2021-08-09 15:30:25 -0400

    included the background image for each instrument

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ var numberOfButtons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < numberOfButtons.length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got clicked.!");
+        var audio = new Audio("assets/sounds/tom-1.mp3");
+        audio.play();
     });
 
 }
